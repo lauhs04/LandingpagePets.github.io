a1425e93f3b542818110fe1d58809081a236e505
laura <laura.hoyos.suarez@gmail.com>
2022-03-19 20:15:39 -0500

    dark/light

--- js/main.js
+++ js/main.js
@@ -59,3 +59,62 @@ function scrollTop(){
 }
 
 window.addEventListener('scroll', scrollTop);
+
+/*=== dark light theme ===*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-toggle-right';
+
+// previously selected topic (if usr selected)
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// we obtain the current theme that the interface has by validating the dark-theme class
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
+
+// we validate if the user previously chose a topic
+if(selectedTheme) {
+    // if the validation is fullfilled, we ask what the issue was to know if we activated or deactivated the dark 
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme);
+}
+
+//Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // we save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/* === scroll reveal animation === */
+
+const sr = ScrollReveal({
+    ditance: '30px',
+    duration: 1800,
+    reset: true
+});
+
+sr.reveal(`.home_data, .home_img, .decoration_data, accesory_content, footer_content`, {
+                origin: 'top',
+                interval: 200
+            })
+
+sr.reveal('.share_img, send_content', {
+    origin: 'left'
+})
+
+sr.reveal('.share_data, send_img', {
+    origin: 'right'
+})
+
+function buy () {
+alert("hola a todos")
+}
+
+buy()
