059743b7ab52f51f4d463bee8da68fe3803c6111
laura <laura.hoyos.suarez@gmail.com>
2022-03-21 00:28:02 -0500

    change style

--- js/main.js
+++ js/main.js
@@ -1,120 +1,113 @@
-/*===Show menu===*/
-const showMenu = (toggleId, navId) => {
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
-
+    
     // Validate that variables exist
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('show-menu')
         })
     }
 }
+showMenu('nav-toggle','nav-menu')
 
-showMenu('nav-toggle','nav-menu');
-
-/*===remove menu mobile===*/
-const navLink = document.querySelectorAll('.nav_link')
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*===scroll sections active link===*/
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-function scroollActive(){
+function scrollActive(){
     const scrollY = window.pageYOffset
 
-    sections.forEach(current => {
+    sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
-        } else {
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
-
 window.addEventListener('scroll', scrollActive)
 
-/*===change background header===*/
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
 function scrollHeader(){
     const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
-
 window.addEventListener('scroll', scrollHeader)
 
-/*=== show scroll top ===*/
+/*==================== SHOW SCROLL TOP ====================*/ 
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
+window.addEventListener('scroll', scrollTop)
 
-window.addEventListener('scroll', scrollTop);
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-toggle-right'
 
-/*=== dark light theme ===*/
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'bx-toggle-right';
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
 
-// previously selected topic (if usr selected)
-
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
-
-// we obtain the current theme that the interface has by validating the dark-theme class
-
+// We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
 
-// we validate if the user previously chose a topic
-if(selectedTheme) {
-    // if the validation is fullfilled, we ask what the issue was to know if we activated or deactivated the dark 
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-    themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme);
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
 }
 
-//Activate / deactivate the theme manually with the button
+// Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // add or remove the dark / icon theme
+    // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // we save the theme and the current icon that the user chose
+    // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
-/* === scroll reveal animation === */
-
+/*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
-    ditance: '30px',
+    distance: '30px',
     duration: 1800,
-    reset: true
+    reset: true,
 });
 
-sr.reveal(`.home_data, .home_img, .decoration_data, accesory_content, footer_content`, {
-                origin: 'top',
-                interval: 200
-            })
+sr.reveal(`.home__data, .home__img, 
+           .decoration__data,
+           .accessory__content,
+           .footer__content`, {
+    origin: 'top',
+    interval: 200,
+})
 
-sr.reveal('.share_img, send_content', {
+sr.reveal(`.share__img, .send__content`, {
     origin: 'left'
 })
 
-sr.reveal('.share_data, send_img', {
+sr.reveal(`.share__data, .send__img`, {
     origin: 'right'
 })
-
-function buy () {
-alert("hola a todos")
-}
-
-buy()
